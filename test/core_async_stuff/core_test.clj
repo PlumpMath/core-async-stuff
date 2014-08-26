c0e23e132c17947db392b4cc6cd02fad458450ac
Toby James <tjames221188@hotmail.com>
2014-08-26 18:47:06 +0100

    Initial commit

---
+++ test/core_async_stuff/core_test.clj
@@ -0,0 +1,7 @@
+(ns core-async-stuff.core-test
+  (:require [clojure.test :refer :all]
+            [core-async-stuff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
